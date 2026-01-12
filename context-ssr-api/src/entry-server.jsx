52e6e4aeaa7b1511f69a2504db420906cd07caba
saleha18 <saleha.a.ali@accenture.com>
2026-01-12 07:19:36 +0000

    context ssr api creation

--- context-ssr-api/src/entry-server.jsx
+++ context-ssr-api/src/entry-server.jsx
@@ -3,9 +3,8 @@ import { UserProvider } from "./context/UserContext";
 import App from './App';
 
 export async function render() {
-    const response = await fetch("http://localhost:5173/api/user");
+    const response = await fetch("http://localhost:5173/ssr-users");
     const user = await response.json();
-
     const html = renderToString(
         <UserProvider initialUser={user}>
             <App />
